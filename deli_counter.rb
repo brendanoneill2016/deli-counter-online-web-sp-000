f90ead41d6bff8e8053d235949cc415d5567ba6c
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-16 08:10:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  
+if katz_deli != []
+  
+katz_deli.each_with_index { |name, index| puts "#{index + 1}: #{name}"}
+
+
+else
+  puts "The line is currently empty."
+end
+
+end
+
+def take_a_number(katz_deli,name)
+  position = katz_deli.count + 1 
+  puts "#{name}: you are position number #{position} in the queue."
+end
